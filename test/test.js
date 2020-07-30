3278dc3643c903fecd23a734865e75a5ce45e8ba
DrD <derekjonp@gmail.com>
2020-07-30 00:35:57 -0400

    v0.1.5

--- test/test.js
+++ test/test.js
@@ -10,11 +10,5 @@ async function build() {
     buld.setSrc("./test/view/nothing.html")
     buld.setEvent('load')
     var file2 = await buld.buildToString({data:"hi2"})
-    console.log("file2: "+file2)
-    //var html2 = await buld.buildToString();
-    // if(html && html2){
-    //    console.log("test passed");
-    //TODO acutally get some testing
-    //}
 }
 
